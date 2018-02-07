1fd8ee5db3d05f2a4816084c3e804761e6cf89e2
David Potsiadlo <davidpots@gmail.com>
2018-02-07 11:31:34 -0600

    Adds first network page

---
+++ network--anytime-fitness.js
@@ -0,0 +1,54 @@
+var reviews = [
+  // {
+  //   name:      "_____",
+  //   stars:     "_____",
+  //   text:      "_____",
+  //   url:       "_____",
+  //   loc:       "_____",
+  //   avatar:    "_____",
+  //   biz:       "_____",
+  //   platform:  "_____"
+  // },
+
+  {
+    name:      "Alex S.",
+    stars:     "1",
+    text:      "Won't let let you cancel your membership. Takes money directly out of your bank account even if you injure yourself and physically can not lift weights anymore.",
+    url:       "https://www.yelp.com/biz/anytime-fitness-austin?hrid=mwFcKmfzURaHTtHm5m8nqg&utm_campaign=www_review_share_popup&utm_medium=copy_link&utm_source=(direct)",
+    loc:       "Austin, TX",
+    avatar:    "_____",
+    biz:       "Anytime Fitness",
+    platform:  "Yelp"
+  },
+  {
+    name:      "Brian K.",
+    stars:     "1",
+    text:      "What a terrible owner. I canceled my membership within three days after signing up and they kept taking money out of my account for months and months and months without my authorization.",
+    url:       "https://www.yelp.com/biz/anytime-fitness-phoenix-4?hrid=-jkOCIK1OBQKn_yfWAMKaQ&utm_campaign=www_review_share_popup&utm_medium=copy_link&utm_source=(direct)",
+    loc:       "Phoenix, AZ",
+    avatar:    "_____",
+    biz:       "Anytime Fitness",
+    platform:  "Yelp"
+  },
+  {
+    name:      "Carlen L.",
+    stars:     "5",
+    text:      "This is a great branch - very clean, well maintained, and has lots of options (punching bag, Jacob's ladder, etc.) Plenty of space and never too crowded. Everyone actually does rerack their weights here.",
+    url:       "https://www.yelp.com/biz/anytime-fitness-austin?hrid=7LwQIyGRZmU9-MMSFh2xbw&utm_campaign=www_review_share_popup&utm_medium=copy_link&utm_source=(direct)",
+    loc:       "Austin, TX",
+    avatar:    "_____",
+    biz:       "Anytime Fitness",
+    platform:  "Yelp"
+  },
+  {
+    name:      "Sean S.",
+    stars:     "5",
+    text:      "Very clean A+. 24hour access. Friendly staff. Good equipment. Idk what's not to like about this place. Especially with the new renovations, makes it look even nicer!",
+    url:       "https://www.yelp.com/biz/anytime-fitness-phoenix-2?hrid=18JfDaunbEYiBH66j6K8Bw&utm_campaign=www_review_share_popup&utm_medium=copy_link&utm_source=(direct)",
+    loc:       "Phoenix, AZ",
+    avatar:    "_____",
+    biz:       "Anytime Fitness",
+    platform:  "Yelp"
+  },
+
+]
