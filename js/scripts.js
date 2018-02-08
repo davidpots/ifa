38df4242b2e50acd4a0641ad8cc49cbe28e8ff42
David Potsiadlo <davidpots@gmail.com>
2018-02-08 17:00:38 -0600

    fixes type speed

--- js/scripts.js
+++ js/scripts.js
@@ -85,7 +85,7 @@ function loadNewReview() {
 
     new TypeIt('.review-text', {
        strings: reviews[currentReview].text,
-       speed: 10,
+       speed: 35,
        autoStart: true,
        callback: showNoResponseText
     });
