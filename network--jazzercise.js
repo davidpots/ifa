586962a582933a0ba871ee0ea11017992196b146
David Potsiadlo <davidpots@gmail.com>
2018-02-08 14:21:04 -0600

    Finishes all network pages

--- network--jazzercise.js
+++ network--jazzercise.js
@@ -1,41 +1,31 @@
 var reviews = [
 
   {
-    name:      "_____",
-    stars:     "_____",
-    text:      "_____",
-    url:       "_____",
-    loc:       "_____",
+    name:      "Natalie V.",
+    stars:     "1",
+    text:      "Definitely not an ideal workout studio. You use weights and mats for the class and their options are super outdated and not hygienic. If you want something low impact and mundane, this is your jam. ",
+    url:       "https://www.yelp.com/biz/jazzercise-sunnyvale-2?hrid=SY2mXLJBPR7l7ft6byoeWw&utm_campaign=www_review_share_popup&utm_medium=copy_link&utm_source=(direct)",
+    loc:       "San Jose, CA",
     avatar:    "_____",
     biz:       "Jazzercise",
     platform:  "Yelp"
   },
   {
-    name:      "_____",
-    stars:     "_____",
-    text:      "_____",
-    url:       "_____",
-    loc:       "_____",
+    name:      "Marjorie S.",
+    stars:     "5",
+    text:      "I never thought I could be hooked on exercise until I joined this Center. Thirteen years later, I'm stronger than ever and I'm certain I've added years to my life. Unpretentious, very welcoming, and some great dance moves too!",
+    url:       "https://www.yelp.com/biz/jazzercise-denver-center-denver?hrid=KMR2EQXL90WxVINnXba2AQ&utm_campaign=www_review_share_popup&utm_medium=copy_link&utm_source=(direct)",
+    loc:       "Denver, CO",
     avatar:    "_____",
     biz:       "Jazzercise",
     platform:  "Yelp"
   },
   {
-    name:      "_____",
-    stars:     "_____",
-    text:      "_____",
-    url:       "_____",
-    loc:       "_____",
-    avatar:    "_____",
-    biz:       "Jazzercise",
-    platform:  "Yelp"
-  },
-  {
-    name:      "_____",
-    stars:     "_____",
-    text:      "_____",
-    url:       "_____",
-    loc:       "_____",
+    name:      "Helen J.",
+    stars:     "5",
+    text:      "Jazzercise is a GREAT workout!!! Great instructors and good for any age group. Lots of classes at different times of the day.",
+    url:       "https://www.yelp.com/biz/jazzercise-lakeway?hrid=XhmVMcChbKkKMPpSzNNJ-w&utm_campaign=www_review_share_popup&utm_medium=copy_link&utm_source=(direct)",
+    loc:       "Austin, TX",
     avatar:    "_____",
     biz:       "Jazzercise",
     platform:  "Yelp"
--- network--massage-envy-franchising.js
+++ network--massage-envy-franchising.js
@@ -1,41 +1,31 @@
 var reviews = [
 
   {
-    name:      "_____",
-    stars:     "_____",
-    text:      "_____",
-    url:       "_____",
-    loc:       "_____",
+    name:      "Lynda W.",
+    stars:     "1",
+    text:      "Requested appointment online and 26 hours later hadn't heard back although confirmation email said I would hear back shortly. Called and got voice mail. Life's too short for this hassle so moving on to someone else.",
+    url:       "https://www.yelp.com/biz/massage-envy-montgomery-plaza-fort-worth?hrid=ARDXnVMJsE8r6LQ7hvJltA&utm_campaign=www_review_share_popup&utm_medium=copy_link&utm_source=(direct)",
+    loc:       "Fort Worth, TX",
     avatar:    "_____",
     biz:       "Massage Envy Franchising",
     platform:  "Yelp"
   },
   {
-    name:      "_____",
-    stars:     "_____",
-    text:      "_____",
-    url:       "_____",
-    loc:       "_____",
+    name:      "Jen O.",
+    stars:     "1",
+    text:      "Worst place ever trying to make an appointment is impossible and when you finally do it's just ok...",
+    url:       "https://www.yelp.com/biz/massage-envy-commack-commack?hrid=jKyV3ZdhWW0xK5rLLfHuQQ&utm_campaign=www_review_share_popup&utm_medium=copy_link&utm_source=(direct)",
+    loc:       "Greenlawn, NY",
     avatar:    "_____",
     biz:       "Massage Envy Franchising",
     platform:  "Yelp"
   },
   {
-    name:      "_____",
-    stars:     "_____",
-    text:      "_____",
-    url:       "_____",
-    loc:       "_____",
-    avatar:    "_____",
-    biz:       "Massage Envy Franchising",
-    platform:  "Yelp"
-  },
-  {
-    name:      "_____",
-    stars:     "_____",
-    text:      "_____",
-    url:       "_____",
-    loc:       "_____",
+    name:      "Lori F.",
+    stars:     "5",
+    text:      "Had a facial, it was great, my face is glowing. Go see Brittany, she was amazing.",
+    url:       "https://www.yelp.com/biz/massage-envy-commack-commack?hrid=miuN_eEv_neiz0A4D-xIFw&utm_campaign=www_review_share_popup&utm_medium=copy_link&utm_source=(direct)",
+    loc:       "Smithtown, NY",
     avatar:    "_____",
     biz:       "Massage Envy Franchising",
     platform:  "Yelp"
